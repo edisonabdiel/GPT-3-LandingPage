02802bf01720f22af0c32ac118f2f77f48f64c1a
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-16 14:47:24 +0100

    feat: Add Header and its respective styles. Add Navbar and its respective styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+//Custom Containers
+import { Footer, Blog, Features, WhatGPT3, Header, Possibility } from './containers';
+//Custom Components
+import { CTA, Brand, Navbar } from './components';
+//Styles
+import './App.css';
+
 
 const App = () => {
     return (
-        <div>
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
             <h1>GTP-3</h1>
         </div>
     )
